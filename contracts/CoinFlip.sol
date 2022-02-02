c755b8073a83149fa0953b8e35bc66fa6a9fb5ff
Nick M <ndplay@ymail.com>
2022-02-02 11:40:22 +0400

    Update CoinFlip.sol

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -9,20 +9,41 @@ contract CoinFlip {
     //0.1 eth by default
     uint128 betAmountInGwei = 100000000000000000;
     //Player addresses
-    address playerOne;
-    address playerTwo;
+    address playerHeads;
+    address playerTails;
+
+    constructor() {
+        owner = payable(msg.sender); // setting the contract creator
+    }
 
     function updateBetAmount(uint128 newBetAmount) external onlyOwner {
         betAmountInGwei = newBetAmount;
     }
 
     //Determine better's order and process the bet.
-    function ProcessBet(address betAdress) public payable {
+    function AcceptBet() public {
         if(currentBetCount == 0)
         {
             require(msg.value == betAmountInGwei);
-            //
+            playerHeads = msg.sender;
+            currentBetCount++;
+        } 
+        else if(currentBetCount == 1)
+        {
+            require(msg.value == betAmountInGwei);
+            playerTails = msg.sender;
+            currentBetCount++;
         }
+        if(currentBetCount == 2)
+        {
+            ProcessBet();
+        }
+    }
+
+    function ProcessBet() internal payable {
+        require(playerHeads != 0);
+        require(playerTails != 0);
+        
     }
 
 }
