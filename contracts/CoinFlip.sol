27757642f88fd2ad23130b075db7bedb025873fe
Nick M <ndplay@ymail.com>
2022-02-06 13:51:14 +0400

    Update CoinFlip.sol

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -2,8 +2,9 @@
 pragma solidity >=0.4.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CoinFlip {
+contract CoinFlip is Ownable {
 
     uint8 currentBetCount = 0;
     //0.1 eth by default
@@ -11,9 +12,13 @@ contract CoinFlip {
     //Player addresses
     address playerHeads;
     address playerTails;
+    //Winner index
+    uint8 winningPlayerIndex;
+    //Contract Owner
+    address owner;
 
     constructor() {
-        owner = payable(msg.sender); // setting the contract creator
+        owner = msg.sender; // setting the contract creator
     }
 
     function updateBetAmount(uint128 newBetAmount) external onlyOwner {
@@ -21,29 +26,42 @@ contract CoinFlip {
     }
 
     //Determine better's order and process the bet.
-    function AcceptBet() public {
+    function acceptBet() public {
         if(currentBetCount == 0)
         {
             require(msg.value == betAmountInGwei);
+            //
             playerHeads = msg.sender;
             currentBetCount++;
         } 
         else if(currentBetCount == 1)
         {
             require(msg.value == betAmountInGwei);
+            
             playerTails = msg.sender;
             currentBetCount++;
         }
         if(currentBetCount == 2)
         {
-            ProcessBet();
+            processBet();
         }
     }
 
-    function ProcessBet() internal payable {
+    function processBet() internal payable {
         require(playerHeads != 0);
         require(playerTails != 0);
-        
+
+        winningPlayerIndex = random() % 2;
+    }
+
+    function random() private view returns (uint) {
+        //Convert hash to integer
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, 2)));
+    }
+
+    function rewardWinner() internal payable 
+    {
+
     }
 
 }
